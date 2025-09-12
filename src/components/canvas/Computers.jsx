621054d803922b379649b3d61bd2b73696c816ae
might <andrezito32@gmail.com>
2025-09-12 12:23:52 +0100

    testing solution for gltf model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,7 +8,7 @@ import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
 
-  const computer = useGLTF('./desktop_pc/scene.gltf');
+  const computer = useGLTF('/desktop_pc/scene.gltf');
   console.log(isMobile);
   return (
     <mesh>
